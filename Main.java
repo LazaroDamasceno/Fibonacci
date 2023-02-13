9b2bf0b83f4fa67aaaacb75e74e71870a4679741
Lázaro Lawrence Pereira Damasceno <62726118+LazaroDamasceno@users.noreply.github.com>
2023-02-13 01:26:24 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,14 +4,6 @@ import java.util.stream.*;
 public class Main {
     public static void main(String[] args) {
         Scanner data = new Scanner(System.in);
-        System.out.println("Recursive Fibonacci");
-        System.out.println(recursiveFibonacci(4));
-        System.out.println(recursiveFibonacci(5));
-        System.out.println(recursiveFibonacci(6));
-        System.out.println("Fibonacci With Reduce");
-        System.out.println(fibonacciWithReduce(4));
-        System.out.println(fibonacciWithReduce(5));
-        System.out.println(fibonacciWithReduce(6));
     }
 
     public static int recursiveFibonacci(int number) {
